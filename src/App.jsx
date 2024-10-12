e9432f9617f6ba8103237e5f7fd6a363a22a2a7b
Parthgrow <parthagarwalchitkara@gmail.com>
2024-10-12 00:35:47 -0700

    pre-last commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import vector3 from "./assets/3rdvector.png";
+import vector2 from "./assets/2ndvector.png";
+import vector1 from "./assets/1stvector.png";
 import logo from "./assets/logo.png";
 import razor from "./assets/razor.png";
 import startup from "./assets/startup.png";
@@ -10,6 +11,8 @@ import microsoft from "./assets/microsoft.png";
 import india from "./assets/india.png";
 import "./App.css";
 import headerImage from "./assets/headerImage.png";
+import { TiChevronRight } from "react-icons/ti";
+import blueline from "./assets/blueline.png";
 
 function App() {
   return (
@@ -19,10 +22,10 @@ function App() {
           <div>
             <img src={logo} className="h-[80px] w-[200px]" alt="" />
           </div>
-          <div className="flex justify-evenly custom-blue gap-3 pt-6">
-            <p className="px-2">Learn</p>
-            <p className="px-2">Practice</p>
-            <p className="px-2">Assignments</p>
+          <div className="flex  custom-blue gap-10 justify-around  pt-6 font-medium">
+            <p className="">Learn</p>
+            <p className="">Practice</p>
+            <p className="">Assignments</p>
           </div>
           <div className=" pt-5">
             <button className="text-[14px] px-4 py-2 rounded-md mx-2 text-custom-blue border-2 border-custom-blue">
@@ -33,37 +36,63 @@ function App() {
             </button>
           </div>
         </nav>
-        <header className="flex justify-center gap-5 ">
-          <div className="border-2 border-red-600">
-            <div className="border-2 border-black flex flex-col w-[550px] ">
-              <div className=" flex justify-evenly text-[14px] font-medium text-custom-blue">
-                <p> Students </p>
-                <p>Teachers </p>
-                <p>Parents</p>
+        <header className="flex min-h-screen py-[20px]  ">
+          <div className=" ml-[150px] mr-[100px] h-[425px] mt-8">
+            <div className=" flex flex-col w-[450px] ">
+              <div className=" flex justify-start gap-6 text-[14px] font-medium text-custom-blue">
+                <div className="flex items-center gap-2">
+                  <div className="bg-custom-vector1 h-4 w-4 items-center flex justify-center rounded-sm">
+                    <img src={vector1} alt="" className="ml-[1px]" />
+                  </div>
+
+                  <p> Students </p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <div className="bg-custom-vector2 h-4 w-4 items-center flex justify-center rounded-sm">
+                    <img src={vector2} className="ml-[1px]" />
+                  </div>
+
+                  <p>Teachers </p>
+                </div>
+
+                <div className="flex items-center gap-2">
+                  <div className="bg-custom-vector3 h-4 w-4 items-center flex justify-center rounded-sm">
+                    <img src={vector3} className=" h-[10px] w-[10px]" />
+                  </div>
+
+                  <p>Parents</p>
+                </div>
               </div>
               <div className="text-[48px] font-extrabold text-custom-blue">
                 Beyond Learning, Becoming
+                <div className="relative bottom-[85px] left-[230px]">
+                  <img src={blueline} alt="" className="w-[130px] h-[6px]" />
+                </div>
               </div>
-              <div className="text-[20px] text-custom-blue">
+              <div className="text-[15px] font-medium  text-custom-heading opacity-80 my-8">
                 Say goodbye to stressful study sessions. With Hyggex's
                 AI-powered learning paths, you can improve your grades and
                 well-being in less time, with less stress.
               </div>
-              <div>
-                <div>
-                  <button className="bg-custom-lightBlue text-white px-4 py-2 rounded-xl">
-                    Start Learning
+              <div className="mt-4">
+                <div className="flex gap-2 mb-4">
+                  <button className="bg-custom-lightBlue text-white px-4 py-2  rounded-xl flex">
+                    Start Learning{" "}
+                    <TiChevronRight className="mt-[5.5px] ml-2" />
                   </button>
-                  <button className="bg-custom-lightBlue  text-black px-4 py-2 rounded-xl">
+                  <button className="bg-blue-100 text-custom-lightBlue px-4 py-2 rounded-xl">
                     Book a demo
                   </button>
                 </div>
-                <div>
-                  <p>Akash has scored 9/10 in self awareness</p>
-                  <p>Just Now </p>
+                <div className="text-sm">
+                  <p>
+                    <span className="text-custom-lightBlue">Akash</span> has
+                    scored <span className="text-custom-lightBlue">9/10 </span>
+                    in self awareness .{" "}
+                    <span className="text-custom-lightBlue">Just Now </span>
+                  </p>
                 </div>
               </div>
-              <div className="border-2 border-black"></div>
             </div>
           </div>
           <div className="flex flex-col">
@@ -106,45 +135,68 @@ function App() {
           ></div> */}
           </div>
         </header>
-        <div className="flex items-center justify-around px-[70px] ">
-          <div>Trusted by the best in the industry</div>
-          <div>
-            <img src={razor} alt="" className=" h-[33px] w-[177px]" />
-          </div>
-          <div>
-            <img
-              src={startup}
-              alt=""
-              className=""
-              style={{ objectFit: "contain", height: "100px", width: "100px" }}
-            />
-          </div>
-          <div>
-            <img src={knight} alt="" className="w-[70px]" />
-          </div>
-          <div>
-            <img src={wadhwani} alt="" className="" />
-          </div>
-          <div>
-            {" "}
-            <img src={microsoft} alt="" className="" />
-          </div>
-          <div>
-            <img src={india} className="h-[100px] w-[150px]" />
+        <div
+          className=" border-black   "
+          style={{ backgroundColor: "rgba(64, 127, 255,0.035)" }}
+        >
+          <div className="flex items-center justify-around px-[70px]  ">
+            <div className="w-[92px]">
+              <span className="text-custom-darkBlue font-bold">
+                Trusted by the best
+              </span>
+              <span className="text-gray-400 font-bold"> in the industry</span>
+            </div>
+            <div>
+              <img src={razor} alt="" className=" h-[33px] w-[177px]" />
+            </div>
+            <div>
+              <img
+                src={startup}
+                alt=""
+                className=""
+                style={{
+                  objectFit: "contain",
+                  height: "100px",
+                  width: "100px",
+                }}
+              />
+            </div>
+            <div>
+              <img src={knight} alt="" className="w-[70px]" />
+            </div>
+            <div>
+              <img src={wadhwani} alt="" className="" />
+            </div>
+            <div>
+              {" "}
+              <img src={microsoft} alt="" className="" />
+            </div>
+            <div>
+              <img src={india} className="h-[100px] w-[150px]" />
+            </div>
           </div>
         </div>
         <div className="bg-custom-darkBlue min-h-screen ">
-          <div className="border-2 border-red-700">
-            <div className="border-4 border-white ">
-              See <span> Adaptive Learning </span> in Action
+          <div className="border-2 border-red-700 flex items-center flex-col py-40 ">
+            <div className=" text-[40px] mb-28 font-bold ">
+              <span className="text-white ">See </span>
+              <span className="text-custom-lightBlue">Adaptive Learning</span>
+              <span className="text-white"> in Action</span>
             </div>
-            <div className="bg-white h-[519px] w-[1024px] text-black rounded-[16px] border-[1px] border-white">
+            <div className="bg-white h-[519px] w-[1024px] text-black rounded-[16px] border-[1px] border-white ">
               <div className="flex gap-3 ml-[16px] mt-[20px]">
                 <div className="rounded-full bg-red-600 h-2 w-2"></div>
                 <div className="rounded-full bg-yellow-400 h-2 w-2"></div>
                 <div className="rounded-full bg-green-600 h-2 w-2"></div>
               </div>
-              <div className="h-[453px] w-[992px] bg-black"></div>
+              <div className="h-[453px] w-[992px] bg-black ml-[16px] mt-[15px]">
+                <iframe
+                  // src="https://www.youtube.com/watch?v=88NJP9j-qNo&list=PLgyJAK8noDUG81VJAP38cF9GVRVKLipmZ"
+                  width="100%"
+                  height="100%"
+                  src="https://www.youtube.com/watch?v=88NJP9j-qNo&list=PLgyJAK8noDUG81VJAP38cF9GVRVKLipmZ"
+                ></iframe>
+              </div>
             </div>
           </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,12 @@ export default {
       colors : {
         "custom-blue" : "#06286E",
         "custom-lightBlue" : "#407FFF",
-        "custom-darkBlue" : "#06286E"
+        "custom-darkBlue" : "#06286E",
+        "lightest-blue" : "#407FFF33",
+        "custom-heading" : "#0F2669CC",
+        "custom-vector1" : "#A5C3FF99",
+        "custom-vector2" : "#FFEDAD99",
+        "custom-vector3" : "#B7A8FF99"
       }
     },
   },
